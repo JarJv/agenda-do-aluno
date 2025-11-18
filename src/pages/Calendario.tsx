6fa97c108fe859ff5b22290bf5f117cb35602600
vitomazeli <142349519+vitomazeli@users.noreply.github.com>
2025-11-18 03:25:22 +0000

    Foi iniciada a página de Calendario

--- src/pages/Calendario.tsx
+++ src/pages/Calendario.tsx
@@ -1,9 +1,35 @@
 import '../assets/style.css'
+import BlueFilledButtom from'../components/BlueFilledButton'
+import BordedButton from '../components/BordedButton.tsx'
+
+
 
 export default function Calendario(){
     return(
+
+    <div> 
+    
         <div>
-            <h1>Opa, mundo!</h1>
+            <BlueFilledButtom>
+                Março
+                </BlueFilledButtom>  
+                
+                    
+        
         </div>
+
+        <div id="calendario-container">
+            <p className="text-white text-lg font-bold">Março</p>
+                        
+        </div>
+
+        <div id="legenda-calendario">
+            <p className="text-green-400 text-lg font-bold">Letivo</p>
+            <p className="text-yellow-300 text-lg font-bold">Falta</p>
+            <p className="text-red-600 text-lg font-bold">Não letivo</p>
+        </div>
+
+
+    </div>    
     )
 }
