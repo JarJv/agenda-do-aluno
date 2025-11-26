7fdd72d30865eddc8fd1a9b2d9a55857a752927a
Victor Baccon Vieira <victorbacconvieira@gmail.com>
2025-11-26 07:52:06 +0000

    Foi adicionada a função de navegação para os dois botões da NavBottom com os caminhos que eles devem seguir

--- src/components/NavBottom.tsx
+++ src/components/NavBottom.tsx
@@ -6,8 +6,8 @@ export default function NavBottom(){
     return(
         <div className='fixed bottom-0 left-0 right-0 bg-(--c1)'>
             <nav className='flex justify-between items-center px-6 py-8'>
-                <SignOut color='#fff' size={32} weight='bold' className='transform duration-100 active:scale-120 cursor-pointer'/>
-                <House color='#fff' size={32} weight='fill' className='transform duration-100 active:scale-120 cursor-pointer'/>
+                <a href="/"><SignOut href="/menu" color='#fff' size={32} weight='bold' className='transform duration-100 active:scale-120 cursor-pointer'/></a>
+                <a href="/menu"><House href="/menu" color='#fff' size={32} weight='fill' className='transform duration-100 active:scale-120 cursor-pointer'/></a>
                 <AccessibilityMenu/>
             </nav>
         </div>
