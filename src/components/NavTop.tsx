166be233ac0eb53577a15ac44628c921df241677
Victor J. <79714103+JarJv@users.noreply.github.com>
2025-11-26 04:51:43 +0000

    Fix: botões alinhados na esquerda

--- src/components/NavTop.tsx
+++ src/components/NavTop.tsx
@@ -11,7 +11,7 @@ function NavTop() {
 
     return (
        <div className="relative w-full">
-            <nav className="bg-(--c2) w-full py-4 px-5 flex items-center rounded-none justify-between">
+            <nav className="bg-(--c2) w-full py-4 px-5 flex items-start rounded-none justify-between">
                 <p className="text-white text-lg">Bem-vindo, {""}
                     <label id="nomeAluno" className="font-bold">
                         Fulana
@@ -38,52 +38,52 @@ function NavTop() {
                 <ul className="flex flex-col gap-4 p-4 text-base">
                     {/*Exemplo de tela selecionada*/}
                     
-                    <div className='has-checked:bg-indigo-50 has-checked:text-indigo-900 has-checked:ring-indigo-200 flex items-center py-1 gap-2 bg-white rounded-full justify-center'>
+                    <div className='has-checked:bg-indigo-50 has-checked:text-indigo-900 has-checked:ring-indigo-200 flex items-center py-1 gap-2 bg-white rounded-full px-4'>
                         <House size={28} color="#797FF2" />
                         <a href="/menu"><li className="text-(--c2) font-medium">Menu</li></a>
                     </div>
 
-                    <div className='has-checked:bg-indigo-50 has-checked:text-indigo-900 has-checked:ring-indigo-200 flex items-center py-1 gap-2 justify-center'>
+                    <div className='has-checked:bg-indigo-50 has-checked:text-indigo-900 has-checked:ring-indigo-200 flex items-center py-1 gap-2 px-4'>
                         <CalendarDots size={28} color="#ffffff" />
                         <a href="/calendario"><li className="text-white font-medium">Calendário</li></a> 
                     </div>
                     
-                    <div className='flex items-center py-1 gap-2 justify-center'>
+                    <div className='flex items-center py-1 gap-2 px-4'>
                         <ClockCountdown size={28} color="#ffffff" />
                         <a href="/horario"><li className="text-white font-medium">Horário</li></a>
                     </div>
                     
-                    <div className='flex items-center py-1 gap-2 justify-center'>
+                    <div className='flex items-center py-1 gap-2 px-4'>
                         <ChalkboardTeacher size={28} color="#ffffff" />
                         <a href="/professores"><li className="text-white font-medium">Professores</li></a>
                     </div>
                     
-                    <div className='flex items-center py-1 gap-2 justify-center'>
+                    <div className='flex items-center py-1 gap-2 px-4'>
                         <Student size={28} color="#ffffff" />
                         <a href="/alunos"><li className="text-white font-medium">Alunos</li></a>
                     </div>
                     
-                    <div className='flex items-center py-1 gap-2 justify-center'>
+                    <div className='flex items-center py-1 gap-2 px-4'>
                         <CheckSquareOffset size={28} color="#ffffff" />
                         <a href="/frequencia"><li className="text-white font-medium">Frequência</li></a>
                     </div>
 
-                    <div className='flex items-center py-1 gap-2 justify-center'>
+                    <div className='flex items-center py-1 gap-2 px-4'>
                         <NotePencil size={28} color="#ffffff" />
                         <a href="/notas"><li className="text-white font-medium">Notas</li></a>
                     </div>
                     
-                    <div className='flex items-center py-1 gap-2 justify-center'>
+                    <div className='flex items-center py-1 gap-2 px-4'>
                         <Notepad size={28} color="#ffffff" />
                         <a href="/anotacoes"><li className="text-white font-medium">Anotações</li></a>
                     </div>
                     
-                    <div className='flex items-center py-1 gap-2 justify-center'>
+                    <div className='flex items-center py-1 gap-2 px-4'>
                         <CircleUserRound size={28} color='#ffffff'/>
                         <a href="/perfil"><li className="text-white font-medium">Perfil</li></a>
                     </div>
                     
-                    <div className='flex items-center py-1 gap-2 justify-center'>
+                    <div className='flex items-center py-1 gap-2 px-4'>
                         <GearSix size={28} color="#ffffff" />
                         <a href="/configuracoes"><li className="text-white font-medium">Configurações</li></a>
                     </div>
