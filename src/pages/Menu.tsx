5d4d7cee975329e24e1f0b36b6a7686cc1bc24b0
Victor Baccon Vieira <victorbacconvieira@gmail.com>
2025-11-26 08:29:34 +0000

    Foi arrumada a navegação do menu, todos os botões levam aos seus respectivos caminhos. Foi adicionado um botão de Configurações aos botões do menu, com seu caminho funcionando normalmente

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -12,41 +12,26 @@ import {
   MoreHorizontal,
 } from "lucide-react";
 import { MenuItem } from "../components/MenuItem";
-import  BordedButton from '../components/BordedButton';
 import EmptySection from "../components/EmptySection.tsx";
 
 export default function Menu() {
   return (
     // Container com padding para não colar nas bordas
     <div className="flex flex-col w-full h-full px-6 py-4">
-      {/* 1. Barra de ícones superior (Config e 3 pontos) 
-          Eles ficam DENTRO da página, logo abaixo do Header roxo */}
-      <div className="flex justify-between items-center mb-6 text-white">
-        <button className="p-2 hover:bg-white/10 rounded-full transition-colors">
-          <Settings size={24} />
-        </button>
-        <button className="p-2 hover:bg-white/10 rounded-full transition-colors">
-          <MoreHorizontal size={24} />
-        </button>
-      </div>
 
-      {/* 2. Grid de Botões */}
+      {/* Grid de Botões */}
       <div className="grid grid-cols-2 gap-4">
-        <MenuItem icon={Calendar} label="Calendário" />
-        <MenuItem icon={Clock} label="Horário" />
-        <MenuItem icon={Users} label="Professores" />
-        <MenuItem icon={GraduationCap} label="Alunos" />
-        <MenuItem icon={CheckSquare} label="Frequência" />
-        <MenuItem icon={FileText} label="Notas" />
-        <MenuItem icon={NotebookPen} label="Anotações" />
-        <MenuItem icon={UserCircle} label="Perfil" />
+        <MenuItem href="/calendario" icon={Calendar} label="Calendário"></MenuItem>
+        <MenuItem href="/horario" icon={Clock} label="Horário" />
+        <MenuItem href="/professores" icon={Users} label="Professores" />
+        <MenuItem href="/alunos" icon={GraduationCap} label="Alunos" />
+        <MenuItem href="/frequencia" icon={CheckSquare} label="Frequência" />
+        <MenuItem href="/notas" icon={FileText} label="Notas" />
+        <MenuItem href="/anotacoes" icon={NotebookPen} label="Anotações" />
+        <MenuItem href="/perfil" icon={UserCircle} label="Perfil" />
+        <MenuItem href="/configuracao" icon={Settings} label="Configurações"/>
       </div>
-
-      <div className="flex justify-end mt-8"> 
-        <BordedButton>
-          Sair
-        </BordedButton>
-      </div>
+      
       <EmptySection/>
     </div>
   );
